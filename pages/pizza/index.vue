a70fab35044ba36d95b5c6d42ee1a597a863d48e
Oleg Grigoriev <go.vasac@gmail.com>
2019-12-23 13:16:18 +0300

    /pizza/ page - list of pizza types

---
+++ pages/pizza/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <main>
+        <h2>Choose your pizza</h2>
+
+        <pizza-types></pizza-types>
+    </main>
+</template>
+
+<script>
+    import PizzaTypes from "~/components/PizzaTypes";
+
+    export default {
+        components: {
+            PizzaTypes,
+        },
+    }
+</script>
+
+<style>
+</style>
