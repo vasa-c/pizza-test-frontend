617294726ccda24556645b75031af098673f4c31
Oleg Grigoriev <go.vasac@gmail.com>
2019-12-23 13:45:59 +0300

    Main page: pizza as links

--- components/PizzaTypes.vue
+++ components/PizzaTypes.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="pizza-list">
-        <div v-for="pizza in types" class="pizza-item">
-            <div class="pizza-name">{{ pizza.name }}</div>
-            <div class="pizza-photo"><img :src="pizza.photo" :alt="pizza.name"></div>
-            <div class="pizza-price">&euro;&nbsp;{{ pizza.prices.eur }}</div>
-        </div>
+        <nuxt-link v-for="pizza in types" :to="`/pizza/${pizza.slug}`">
+            <div class="pizza-item">
+                <div class="pizza-name">{{ pizza.name }}</div>
+                <div class="pizza-photo"><img :src="pizza.photo" :alt="pizza.name"></div>
+                <div class="pizza-price">&euro;&nbsp;{{ pizza.prices.eur }}</div>
+            </div>
+        </nuxt-link>
     </div>
 </template>
 
@@ -19,16 +21,24 @@
 </script>
 
 <style lang="scss">
-    .pizza-item {
-        width: 200px;
-        height: 200px;
-        display: inline-block;
-        .pizza-photo img {
-            width: 150px;
-            height: 150px;
+    .pizza-list {
+        .pizza-item {
+            width: 200px;
+            height: 200px;
+            display: inline-block;
+            padding-left: 50px;
+            cursor: pointer;
+            .pizza-photo img {
+                width: 150px;
+                height: 150px;
+            }
+            .pizza-price {
+                font-style: italic;
+            }
+            color: #343a40;
         }
-        .pizza-price {
-            font-style: italic;
+        .pizza-item:hover {
+            background-color: #bbbbbb;
         }
     }
 </style>
