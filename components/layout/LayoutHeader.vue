412eb6ede2b147d6cd042bb67e936496da140bb7
Oleg Grigoriev <go.vasac@gmail.com>
2019-12-23 11:34:26 +0300

    Layout: header & logo

---
+++ components/layout/LayoutHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <div class="wrapper">
+            <nuxt-link to="/">
+                <div id="logo">
+                    PT
+                </div>
+            </nuxt-link>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style>
+    header {
+        background-color: #343a40;
+    }
+    #logo {
+        display: inline-block;
+        color: white;
+        font-size: 32px;
+        margin: 5px 0;
+        padding: 5px;
+        border: 2px solid white;
+        border-radius: 5px;
+    }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,27 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+    <div>
+        <layout-header></layout-header>
+        <nuxt />
+    </div>
 </template>
 
-<style>
+<script>
+    import LayoutHeader from "~/components/layout/LayoutHeader";
+
+    export default {
+        components: {
+            LayoutHeader,
+        },
+    }
+</script>
+
+<style lang="scss">
+    body {
+        margin: 0;
+        font-family: "Ubuntu", sans-serif;
+    }
+    .wrapper {
+        max-width: 1280px;
+        margin: auto;
+    }
 </style>
