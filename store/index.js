a071b44152fb2b262076e68f08073595ed04a0b1
Oleg Grigoriev <go.vasac@gmail.com>
2019-12-23 12:42:35 +0300

    API methods
    - api() for API-request (GET and POST)
    - page() for async load page data
    - CSRF token for POST requests
    - preload layout data on server

---
+++ store/index.js
@@ -0,0 +1,70 @@
+import axios from "axios";
+
+export const state = () => ({
+    cookieOnServer: null,
+    csrf: null,
+});
+
+export const getters = {
+};
+
+export const mutations = {
+};
+
+export const actions = {
+
+    nuxtServerInit({state, dispatch}, context) {
+        if (context.req) {
+            state.cookieOnServer = context.req.headers.cookie || null;
+        }
+
+        return dispatch("page", "layout").then((data) => {
+            state.csrf = data.csrf;
+        });
+    },
+
+    /**
+     * Runs API action
+     *
+     * @param action (for example "layout")
+     * @param data (POST data, for POST request without data - set {}, for undefined - GET-request)
+     */
+    api({state}, {action, data}) {
+        const
+            baseUrl = process.env.baseUrl,
+            url = `${baseUrl}/api/${action}`,
+            options = {
+                url: url,
+            };
+        if (state.cookieOnServer !== null) {
+            options.headers = {
+                Cookie: state.cookieOnServer,
+            };
+        }
+        if (data) {
+            options.method = "post";
+            options.data = {
+                csrf: state.csrf,
+                ...data,
+            };
+        } else {
+            options.method = "get";
+        }
+        return axios(options);
+    },
+
+    /**
+     * Returns data for page
+     *
+     * @param dispatch
+     * @param page (for example, "pizza/chicago")
+     */
+    page({dispatch}, page) {
+        return dispatch("api", {
+            action: page,
+        }).then((response) => {
+            return response.data;
+        });
+    },
+
+};
