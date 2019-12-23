5634a7b09b7eb74de248ba28a99cdbc063571114
Oleg Grigoriev <go.vasac@gmail.com>
2019-12-23 11:59:25 +0300

    Layout: shopping cart and user block

--- components/layout/LayoutHeader.vue
+++ components/layout/LayoutHeader.vue
@@ -6,6 +6,10 @@
                     PT
                 </div>
             </nuxt-link>
+            <div id="user-block">
+                <img src="~/assets/icons/shopping.svg" alt="Shopping cart" class="cart-icon" />
+            </div>
+            <br style="clear:both" />
         </div>
     </header>
 </template>
@@ -27,5 +31,14 @@
         padding: 5px;
         border: 2px solid white;
         border-radius: 5px;
+        float: left;
+    }
+    #user-block {
+        float: right;
+        color: white;
+        padding: 15px;
+        .cart-icon {
+            width: 20px;
+        }
     }
 </style>
